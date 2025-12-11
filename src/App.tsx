26ef984cb6106c576f53bde19a9c2d5267c65702
Nejib Musema <nejibmusema@gmail.com>
2025-12-11 10:41:31 +0300

    chore: Introduce Prettier for code formatting and refine ESLint configuration.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-
 function App() {
-
-
   return (
     <>
-     <div className="p-10 flex flex-col gap-4">
-      <h1 className="text-3xl font-bold">Sports Hub</h1>
-      <div className="flex gap-4">
-
+      <div className="p-10 flex flex-col gap-4">
+        <h1 className="text-3xl font-bold">Sports Hub</h1>
+        <div className="flex gap-4"></div>
       </div>
-     </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
